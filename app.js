17408c47a5936fbffa7f19789854b513a1059736
H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2021-11-09 21:09:24 +0900

    readFileを使ったHTMLファイルの読み込み

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
-let server = http.createServer(
-    (request,response) => {
-        response.setHeader('Content-Type', 'text/html');
-        response.write(`
-            <!DOCTYPE html>
-            <html lang="ja">
-            <head>
-                <meta charset="utf-8">
-                <title>Hello</title>
-            </head>
-            <body>
-                <h1>Hello Node.js!</h1>
-                <p>This is Node.js sample page.</p>
-                <p>これはNode.jsのサンプルページです。</p>
-            </body>
-            </html>
-        `);
-        response.end();
-    }
-);
+let server = http.createServer(getFromClient);
+
 server.listen(3000);
+console.log('Server start!');
+
+// createServerの処理
+function getFromClient (req, res) {
+    request = req;
+    response = res;
+    fs.readFile('./index.html', 'UTF-8', (err, data ) => {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    });
+}
+    
+
