cffb5d1621a42e4367e23cebe662e4af0de1c1a3
H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2021-11-09 20:44:04 +0900

    HTMLで日本語の挿入

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const http = require('http');
 
 let server = http.createServer(
     (request,response) => {
-        response.end(`
-            <html>
+        response.setHeader('Content-Type', 'text/html');
+        response.write(`
+            <!DOCTYPE html>
+            <html lang="ja">
+            <head>
+                <meta charset="utf-8">
+                <title>Hello</title>
+            </head>
             <body>
-                <h1>Hello</h1>
-                <p>Welcome to Node.js</p>
+                <h1>Hello Node.js!</h1>
+                <p>This is Node.js sample page.</p>
+                <p>これはNode.jsのサンプルページです。</p>
             </body>
             </html>
         `);
+        response.end();
     }
 );
 server.listen(3000);
